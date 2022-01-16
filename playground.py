b18791b1f13cfe6182cdc8c1aaa1b3702b2f4400
nickgiouzel <nickgiouzel@gmail.com>
2022-01-16 20:11:04 +0200

    Added doulion-nx implementation

--- playground.py
+++ playground.py
@@ -1,7 +1,7 @@
 from networkx import gnm_random_graph, nx
 import pandas as pd
 
-from advanced_triangle_counting.doulion import doulion_node_iterator
+from advanced_triangle_counting.doulion import doulion_node_iterator, doulion_nx
 from advanced_triangle_counting.simple_triangle_counting import count_triangles
 
 
@@ -9,7 +9,7 @@ def main():
 
     df1 = pd.read_csv("../CA-GrQc2-small.csv")
     G = nx.from_pandas_edgelist(df1, source="FromNodeId", target="ToNodeId")
-    print(f'number of triangles according to doulion: {doulion_node_iterator(G, p=0.5)}')
+    print(f'number of triangles according to doulion: {doulion_nx(G, p=0.1)}')
 
 
 if __name__ == "__main__":
